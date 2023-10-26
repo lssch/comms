8e69b10f722a34d5e1bdfba3691ce18c9a7eaa8f
lssch <ls.schwarz@outlook.com>
2023-10-26 08:25:57 +0200

    Updated to use c++ standard

--- comms.cpp
+++ comms.cpp
@@ -4,14 +4,17 @@
 
 #include "comms.h"
 #include <iostream>
+#include <bitset>
 
-CommsMaster::CommsMaster(comms_packet_t* prx_data, comms_packet_t* ptx_data, robocar_data_t* pdata_) {
-  prx_packet = prx_data;
-  ptx_packet = ptx_data;
-  pdata = pdata_;
+using namespace std;
+
+Comms::Comms(Comms::comms_packet_t *ptr_rx_data, Comms::comms_packet_t *ptr_tx_data, robocar_data_t *ptr_data) {
+  rx_packet = ptr_rx_data;
+  tx_packet = ptr_tx_data;
+  data = ptr_data;
 }
 
-uint8_t CommsMaster::checksum(const uint8_t *data_, uint16_t length) {
+uint8_t Comms::checksum(const uint8_t *data_, uint16_t length) {
   uint8_t sum = 0;
   for (uint16_t i = 0; i < length; ++i) {
     sum += data_[i];
@@ -19,37 +22,125 @@ uint8_t CommsMaster::checksum(const uint8_t *data_, uint16_t length) {
   return sum;
 }
 
-uint8_t CommsMaster::bitMask(uint8_t lower, uint8_t upper) {
-  uint8_t value = 0;
-  for (uint8_t i=lower; i<=upper; i++)
-    value |= 1 << i;
-  return value;
+void CommsMaster::transmit() {
+  printf("Master: TX-PART\n");
+  // Generate a new request header based on the current request
+  comms_packet_header_t header = {
+          .sync = SYNC,
+          .access_request = static_cast<uint16_t>((current_access_request.state << 8) |
+                                                  (current_access_request.parameter << 6) |
+                                                  (current_access_request.data << 4) |
+                                                  (current_access_request.sensor << 2) |
+                                                  (current_access_request.state << 0)),
+          .package_length = 0,
+          .checksum = 0
+  };
+
+  // Copy current values to the tx buffer based on the current request
+  uint16_t tx_index = 0;
+  last_rx_data_length = sizeof(comms_packet_header_t);
+  switch (current_access_request.state) {
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->state, sizeof(state_t));
+      tx_index += sizeof(state_t);
+      break;
+    case COMMS_ACCESS_REQUEST_GET:
+      last_rx_data_length += sizeof(state_t);
+      break;
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+  switch (current_access_request.sensor) {
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->sensor, sizeof(sensor_t));
+      tx_index += sizeof(sensor_t);
+      break;
+    case COMMS_ACCESS_REQUEST_GET:
+      last_rx_data_length += sizeof(sensor_t);
+      break;
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+  switch (current_access_request.data) {
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->data, sizeof(data_t));
+      tx_index += sizeof(data_t);
+      break;
+    case COMMS_ACCESS_REQUEST_GET:
+      last_rx_data_length += sizeof(data_t);
+      break;
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+  switch (current_access_request.parameter) {
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->parameter, sizeof(parameter_t));
+      tx_index += sizeof(parameter_t);
+      break;
+    case COMMS_ACCESS_REQUEST_GET:
+      last_rx_data_length += sizeof(parameter_t);
+      break;
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+  switch (current_access_request.request) {
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->request, sizeof(request_t));
+      tx_index += sizeof(request_t);
+      break;
+    case COMMS_ACCESS_REQUEST_GET:
+      last_rx_data_length += sizeof(request_t);
+      break;
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+
+  // Set the dynamic part of the header
+  header.package_length = sizeof(comms_packet_header_t) + tx_index;
+  header.checksum = checksum(tx_packet->data.buffer, header.package_length);
+  memcpy(tx_packet->header.buffer, header.buffer, sizeof(comms_packet_header_t));
+
+  // Update data for the next request
+  last_access_request = current_access_request;
+
+  cout << "tx_header: ";
+  for (uint32_t i = 0; i < sizeof(comms_packet_header_t); ++i) {
+    cout << +tx_packet->header.buffer[i] << " ";
+  }
+  cout << endl;
+
+  current_tx_data_length = (last_rx_data_length > tx_packet->header.package_length) ? last_rx_data_length : tx_packet->header.package_length;
+  cout << "tx_packet: ";
+  for (uint32_t i = 0; i < current_tx_data_length - sizeof(comms_packet_header_t); ++i) {
+    cout << +tx_packet->data.buffer[i] << " ";
+  }
+  cout << endl;
+
+  cout << "tx_package is " << +tx_packet->header.package_length << " bytes long";
+  if (last_rx_data_length > tx_packet->header.package_length)
+    cout << " Adding " << +last_rx_data_length - tx_packet->header.package_length << " extra Bytes for the rx_packet of the last request";
+  cout << endl;
 }
 
-
 uint8_t CommsMaster::receive() {
-  std::cout << "Master: RX-PART\n";
-  std::cout << "rx_header: ";
+  cout << "Master: RX-PART\n";
+  cout << "rx_header: ";
   for (uint32_t i = 0; i < sizeof(comms_packet_header_t); ++i) {
-    std::cout << rx_packet->header.buffer[i];
+    cout << +rx_packet->header.buffer[i] << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 
-  std::cout << "rx_packet: ";
-  for (uint32_t i = 0; i < MAX_DATA_LENGTH; ++i) {
-    std::cout << rx_packet->data.buffer[i];
+  cout << "rx_packet: ";
+  for (uint32_t i = 0; i < rx_packet->header.package_length - sizeof(comms_packet_header_t); ++i) {
+    cout << +rx_packet->data.buffer[i] << " ";
   }
-  std::cout <<"" << std::endl;
+  cout << endl;
 
-  std::cout <<"rx_package sync is: 0x" << rx_packet->header.sync << std::endl;
-  std::cout <<"rx_package access request is: " << rx_packet->header.access_request << std::endl;
-  std::cout <<"rx_package is " << rx_packet->header.package_length << "bytes long" << std::endl;
-  std::cout <<"rx_package checksum is: " << rx_packet->header.checksum << std::endl;
+  cout << "rx_package is " << +rx_packet->header.package_length << " bytes long" << endl;
 
   // Validate the received package
   uint16_t rx_packet_calculated_checksum = checksum(rx_packet->data.buffer, rx_packet->header.package_length);
-  if (rx_packet->header.sync == SYNC ||
-      rx_packet->header.checksum == rx_packet_calculated_checksum) {
+  if (rx_packet->header.sync == SYNC && rx_packet->header.checksum == rx_packet_calculated_checksum) {
     uint16_t rx_index = 0;
     // Coppy the valid data witch was generated by the access request from the last cycle into the local ram
     if (last_access_request.state == COMMS_ACCESS_REQUEST_GET) {
@@ -72,50 +163,144 @@ uint8_t CommsMaster::receive() {
       memcpy(&data->request, &rx_packet->data.buffer[rx_index], sizeof(request_t));
       rx_index += sizeof(request_t);
     }
-    std::cout <<"RX-PART SUCCESSFUL \n" << std::endl;
+    cout <<"RX-PART SUCCESSFUL \n" << endl;
+    return EXIT_SUCCESS;
   } else {
-    std::cout <<"Dropping curren package... Package is corrupted.";
-    if (rx_packet->header.sync != SYNC) std::cout <<" SYNC byte is invalid";
+    cout <<"Dropping curren package... Package is corrupted.";
+    if (rx_packet->header.sync != SYNC) cout << " SYNC byte is invalid";
     if (rx_packet->header.checksum != rx_packet_calculated_checksum)
-      std::cout <<" Checksum is wrong got: " << rx_packet_calculated_checksum << " expected: " << rx_packet->header.checksum << std::endl;
+      cout << " Checksum is wrong got: " << rx_packet_calculated_checksum << " expected: " << +rx_packet->header.checksum << endl;
   }
 
-  return EXIT_SUCCESS;
+  return EXIT_FAILURE;
 }
 
-uint8_t CommsMaster::transmit() {
-  return EXIT_SUCCESS;
-}
-
-
-
-
-CommsSlave::CommsSlave(comms_packet_t *prx_data, comms_packet_t *ptx_data, robocar_data_t* pdata_) {
-  prx_packet = prx_data;
-  ptx_packet = ptx_data;
-  pdata = pdata_;
-}
-
-uint8_t CommsSlave::checksum(const uint8_t *data_, uint16_t length) {
-  uint8_t sum = 0;
-  for (uint16_t i = 0; i < length; ++i) {
-    sum += data_[i];
+uint8_t CommsSlave::response() {
+  cout << "SLAVE: RX-PART\n";
+  cout << "rx_header: ";
+  for (uint32_t i = 0; i < sizeof(comms_packet_header_t); ++i) {
+    cout << +rx_packet->header.buffer[i] << " ";
   }
-  return sum;
-}
+  cout << endl;
 
-uint8_t CommsSlave::bitMask(uint8_t lower, uint8_t upper) {
-  uint8_t value = 0;
-  for (uint8_t i=lower; i<=upper; i++)
-    value |= 1 << i;
-  return value;
-}
+  cout << "rx_packet: ";
+  for (uint32_t i = 0; i < rx_packet->header.package_length - sizeof(comms_packet_header_t); ++i) {
+    cout << +rx_packet->data.buffer[i] << " ";
+  }
+  cout << endl;
 
+  cout << "rx_package is " << +rx_packet->header.package_length << " bytes long" << endl;
 
-uint8_t CommsSlave::receive() {
-  return EXIT_SUCCESS;
-}
+  // Build the current access type structure
+  comms_access_request_t access_type = {
+          .state = static_cast<comms_access_type_e>((rx_packet->header.access_request & 0b00000000000011)),
+          .sensor = static_cast<comms_access_type_e>((rx_packet->header.access_request & 0b0000000000001100) >> 2),
+          .data = static_cast<comms_access_type_e>((rx_packet->header.access_request & 0b0000000000110000) >> 4),
+          .parameter = static_cast<comms_access_type_e>((rx_packet->header.access_request & 0b0000000011000000) >> 6),
+          .request = static_cast<comms_access_type_e>((rx_packet->header.access_request & 0b0000001100000000) >> 8),
+  };
 
-uint8_t CommsSlave::transmit() {
+  // Validate the incoming data and response accordingly
+  uint8_t calculated_checksum = checksum(rx_packet->data.buffer, rx_packet->header.package_length);
+  if (rx_packet->header.sync != SYNC || rx_packet->header.checksum != calculated_checksum) {
+    cout <<"Dropping curren package... Package is corrupted.";
+    if (rx_packet->header.sync != SYNC) cout << " SYNC byte is invalid";
+    if (rx_packet->header.checksum != calculated_checksum)
+      cout << " Checksum is wrong got: " << calculated_checksum << " expected: " << +rx_packet->header.checksum << endl;
+    return EXIT_FAILURE;
+  }
+
+  uint16_t rx_index = 0, tx_index = 0;
+
+  // Generate a response header
+  comms_packet_header_t header = {
+          .sync = SYNC,
+          .package_length = 0,
+          .checksum = 0
+  };
+
+  switch (access_type.state) {
+    case COMMS_ACCESS_REQUEST_GET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->state, sizeof(state_t));
+      tx_index += sizeof(state_t);
+      break;
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&data->state, &rx_packet->data.buffer[rx_index], sizeof(state_t));
+      rx_index += sizeof(request_t);
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+
+  switch (access_type.sensor) {
+    case COMMS_ACCESS_REQUEST_GET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->sensor, sizeof(sensor_t));
+      tx_index += sizeof(sensor_t);
+      break;
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&data->sensor, &rx_packet->data.buffer[rx_index], sizeof(sensor_t));
+      rx_index += sizeof(request_t);
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+
+  switch (access_type.data) {
+    case COMMS_ACCESS_REQUEST_GET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->data, sizeof(data_t));
+      tx_index += sizeof(data_t);
+      break;
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&data->data, &rx_packet->data.buffer[rx_index], sizeof(data_t));
+      rx_index += sizeof(request_t);
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+
+  switch (access_type.parameter) {
+    case COMMS_ACCESS_REQUEST_GET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->parameter, sizeof(parameter_t));
+      tx_index += sizeof(parameter_t);
+      break;
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&data->parameter, &rx_packet->data.buffer[rx_index], sizeof(parameter_t));
+      rx_index += sizeof(parameter_t);
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+
+  switch (access_type.request) {
+    case COMMS_ACCESS_REQUEST_GET:
+      memcpy(&tx_packet->data.buffer[tx_index], &data->request, sizeof(request_t));
+      tx_index += sizeof(request_t);
+      break;
+    case COMMS_ACCESS_REQUEST_SET:
+      memcpy(&data->request, &rx_packet->data.buffer[rx_index], sizeof(request_t));
+      rx_index += sizeof(request_t);
+    case COMMS_ACCESS_REQUEST_IGNORE:
+      break;
+  }
+
+  // Set the dynamic part of the header
+  header.package_length = sizeof(comms_packet_header_t) + tx_index;
+  header.checksum = checksum(tx_packet->data.buffer, header.package_length);
+
+  memcpy(tx_packet->header.buffer, header.buffer, sizeof(comms_packet_header_t));
+
+  printf("SLAVE: TX-PART\n");
+
+  cout << "tx_header: ";
+  for (uint32_t i = 0; i < sizeof(comms_packet_header_t); ++i) {
+    cout << +tx_packet->header.buffer[i] << " ";
+  }
+  cout << endl;
+
+  cout << "tx_packet: ";
+  for (uint32_t i = 0; i < tx_packet->header.package_length - sizeof(comms_packet_header_t); ++i) {
+    cout << +tx_packet->data.buffer[i] << " ";
+  }
+  cout << endl;
+
+  cout << "tx_package access request is: " << +tx_packet->header.access_request << endl;
+  cout << "tx_package is " << +tx_packet->header.package_length << " bytes long" << endl;
+
   return EXIT_SUCCESS;
 }
--- comms.h
+++ comms.h
@@ -6,79 +6,73 @@
 #define COMMS_H
 
 #include <cstdint>
-#include "types/types.h"
+#include "../types/types.h"
 
-#define CURR 0
-#define OLD 1
 #define SYNC 0xAA
 
-typedef enum _comms_access_type_e {
-  COMMS_ACCESS_REQUEST_IGNORE = 0,
-  COMMS_ACCESS_REQUEST_SET,
-  COMMS_ACCESS_REQUEST_GET,
-} __attribute__((packed)) comms_access_type_e;
-
-typedef struct _comms_access_request_t {
-  comms_access_type_e state;
-  comms_access_type_e sensor;
-  comms_access_type_e data;
-  comms_access_type_e parameter;
-  comms_access_type_e request;
-} __attribute__((packed)) comms_access_request_t;
-
-typedef union _comms_packet_header_t {
-  struct {
-    uint8_t sync;
-    uint16_t access_request;
-    uint16_t package_length;
-    uint8_t checksum;
-  } __attribute__((packed));
-  uint8_t buffer[6];
-} __attribute__((packed)) comms_packet_header_t;
-
-typedef union _comms_packet_t {
-  struct {
-    comms_packet_header_t header;
-    robocar_data_t data;
-  } __attribute__((packed)) ;
-  uint8_t buffer[sizeof(comms_packet_header_t) + sizeof(robocar_data_t)];
-} comms_packet_t;
-
-const uint16_t MAX_DATA_LENGTH = sizeof(robocar_data_t);
-const uint16_t MAX_PACKET_LENGTH = sizeof(comms_packet_header_t) + sizeof(robocar_data_t);
-
-comms_packet_t* rx_packet;
-comms_packet_t* tx_packet;
-robocar_data_t* data;
-
-class CommsMaster {
+class Comms {
 public:
-  CommsMaster(comms_packet_t* prx_data, comms_packet_t* ptx_data, robocar_data_t* pdata_);
-  uint8_t transmit();
-  uint8_t receive();
-private:
-  uint8_t checksum(const uint8_t *data_, uint16_t length);
-  uint8_t bitMask(uint8_t lower, uint8_t upper);
+  typedef enum _comms_access_type_e {
+    COMMS_ACCESS_REQUEST_IGNORE = 0,
+    COMMS_ACCESS_REQUEST_SET,
+    COMMS_ACCESS_REQUEST_GET,
+  } __attribute__((packed)) comms_access_type_e;
 
-  comms_packet_t* prx_packet;
-  comms_packet_t* ptx_packet;
-  robocar_data_t* pdata;
-  _comms_access_request_t last_access_request;
-  _comms_access_request_t current_access_request;
+  typedef struct _comms_access_request_t {
+    comms_access_type_e state;
+    comms_access_type_e sensor;
+    comms_access_type_e data;
+    comms_access_type_e parameter;
+    comms_access_type_e request;
+  } __attribute__((packed)) comms_access_request_t;
+
+  typedef union _comms_packet_header_t {
+    struct {
+      uint8_t sync;
+      uint16_t access_request;
+      uint16_t package_length;
+      uint8_t checksum;
+    } __attribute__((packed));
+    uint8_t buffer[6];
+  } __attribute__((packed)) comms_packet_header_t;
+
+  typedef union _comms_packet_t {
+    struct {
+      comms_packet_header_t header;
+      robocar_data_t data;
+    } __attribute__((packed)) ;
+    uint8_t buffer[sizeof(comms_packet_header_t) + sizeof(robocar_data_t)];
+  } comms_packet_t;
+
+  Comms(comms_packet_t* ptr_rx_data, comms_packet_t* ptr_tx_data, robocar_data_t* ptr_data);
+
+protected:
+  uint8_t checksum(const uint8_t *data_, uint16_t length);
+
+  comms_packet_t* rx_packet;
+  comms_packet_t* tx_packet;
+  robocar_data_t* data;
 };
 
-class CommsSlave {
+class CommsMaster: public Comms {
 public:
-  CommsSlave(comms_packet_t* prx_data, comms_packet_t* ptx_data, robocar_data_t* pdata_);
-  uint8_t transmit();
+  CommsMaster(comms_packet_t* ptr_rx_data, comms_packet_t* ptr_tx_data, robocar_data_t* ptr_data)
+  : Comms(ptr_rx_data, ptr_tx_data, ptr_data) {};
+  void transmit();
   uint8_t receive();
-private:
-  uint8_t checksum(const uint8_t *data_, uint16_t length);
-  uint8_t bitMask(uint8_t lower, uint8_t upper);
 
-  comms_packet_t* prx_packet;
-  comms_packet_t* ptx_packet;
-  robocar_data_t* pdata;
+  _comms_access_request_t current_access_request;
+private:
+  _comms_access_request_t last_access_request;
+  uint8_t current_tx_data_length;
+  uint8_t last_rx_data_length;
+};
+
+class CommsSlave: public Comms {
+public:
+  CommsSlave(comms_packet_t* ptr_rx_data, comms_packet_t* ptr_tx_data, robocar_data_t* ptr_data)
+  : Comms(ptr_rx_data, ptr_tx_data, ptr_data) {};
+  uint8_t response();
 };
 
 #endif //COMMS_H
